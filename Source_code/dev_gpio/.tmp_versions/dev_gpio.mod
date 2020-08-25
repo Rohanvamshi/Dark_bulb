d350aff9743bd7d1d9ad65fac251d906a16b0c16
Rohan <rohanvamshi21@gmail.com>
2020-08-25 05:38:08 +0000

    removed the kernel object files

--- Source_code/dev_gpio/.tmp_versions/dev_gpio.mod
+++
@@ -1,3 +0,0 @@
-/home/pi/Dark_bulb/Source_code/dev_gpio/./src/dev_gpio.ko
-/home/pi/Dark_bulb/Source_code/dev_gpio/./src/dev_gpio.o
-
--- Source_code/dev_gpio/src/.dev_gpio.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/pi/Dark_bulb/Source_code/dev_gpio/./src/dev_gpio.ko := ld -EL -r  -T ./scripts/module-common.lds --build-id  -o /home/pi/Dark_bulb/Source_code/dev_gpio/./src/dev_gpio.ko /home/pi/Dark_bulb/Source_code/dev_gpio/./src/dev_gpio.o /home/pi/Dark_bulb/Source_code/dev_gpio/./src/dev_gpio.mod.o ;  true
